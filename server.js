123f11681899167409baf48451426f78585e92bc
c0san1j <jsvalentin@gmail.com>
2021-08-09 21:35:45 -0400

    Initial Setup

---
+++ server.js
@@ -0,0 +1,37 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+//? Mongoose Connection
+mongoose
+	.connect(process.env.DATABASE_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((result) => {
+		app.listen(process.env.PORT || 5000, () =>
+			console.log(`Listening In Port: ${process.env.PORT}`)
+		);
+		console.log('Connected To Database');
+	})
+	.catch((err) => console.log(err));
+
+// app.listen(, () => {
+// 	console.log(`listening on ${process.env.PORT}`);
+// });
